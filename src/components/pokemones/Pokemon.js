a57d5a0a4a6a313e41b8682e6bbb40465a8bf5fb
GMartin03 <martingomez2003174@hotmail.com>
2022-11-16 19:50:59 -0300

    como obtener los nombres

--- src/components/pokemones/Pokemon.js
+++ src/components/pokemones/Pokemon.js
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react'
 import axios from 'axios'
-
+import { Card } from 'react-bootstrap'
 
 const Pokemon = () => {
 
@@ -8,16 +8,20 @@ const Pokemon = () => {
     getPokemons()
   }, [])
   
-
   const getPokemons = async () => {
-    const { data } = await axios("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=30", {params: {  } })
-    console.log(data.results)
-  } 
+    const  results  = await axios("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=30", {params: {  } })
+    console.log(results.data.results) // Esta es la "ruta" para llegar al pokemon pero no se como llegar solamente al nombre y como deberia almacenar sus nombres
 
+  } 
 
   return (
     <div className='Pokemon_container'>
-      a
+      <Card>
+        <Card.Img/>
+        <Card.Body>
+          <Card.Title></Card.Title>
+        </Card.Body>
+      </Card>
     </div>
   )
 }
